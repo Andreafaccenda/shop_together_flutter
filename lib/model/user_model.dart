2438bca8e0185329700adb39ce7c28279ab72171
aFaccenda <S1096568@studenti.univpm.it>
2023-02-02 11:22:19 +0100

    json user

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,8 +1,17 @@
 class UserModel {
-  String userId, name, email, password;
+  String? userId, name, email, password;
 
   UserModel({required this.userId, required this.name, required this.email, required this.password});
 
+  UserModel.fromJson(Map<dynamic, dynamic> map) {
+    if (map == null) {
+      return;
+    }
+    userId = map['userId'];
+    name = map['name'];
+    email = map['email'];
+    password=map['password'];
+  }
 
   toJson() {
     return {
